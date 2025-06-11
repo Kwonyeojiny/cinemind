ed5627f132affebb0c34d358829f8857fb25dea3
Kwonyeojiny <kangagi0009@naver.com>
2025-06-11 22:38:44 +0900

    refactor: App.tsx에서 MovieList 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import './App.css';
-import MovieCard from './components/MovieCard';
-import movieListData from './data/movieListData.json';
 import MovieDetail from './pages/MovieDetail';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <>
       <MovieDetail />
-      <div className="flex flex-wrap justify-center items-center gap-8">
-        {movieListData.results.map(movie => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            poster_path={movie.poster_path}
-            vote_average={movie.vote_average}
-          />
-        ))}
-      </div>
+      <MovieList />
     </>
   );
 }
---
+++ src/pages/MovieList.tsx
@@ -0,0 +1,19 @@
+import MovieCard from '../components/MovieCard';
+import movieListData from '../data/movieListData.json';
+
+const MovieList = () => {
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-8">
+      {movieListData.results.map(movie => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          vote_average={movie.vote_average}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
