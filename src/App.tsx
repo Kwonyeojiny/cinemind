3a27f6f460a09b5c69c6e99a707030be39773455
Kwonyeojiny <kangagi0009@naver.com>
2025-06-12 13:46:23 +0900

    feat: Layout 컴포넌트 추가 및 Outlet 구조로 App 라우팅 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MovieDetail from './pages/MovieDetail';
 import MovieList from './pages/MovieList';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MovieList />} />
-        <Route path="/details/:id" element={<MovieDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MovieList />} />
+          <Route path="/details/:id" element={<MovieDetail />} />
+        </Route>
       </Routes>
     </>
   );
