6767a06f12cda9f33a0728d6f7212488d7039010
Kwonyeojiny <kangagi0009@naver.com>
2025-06-12 14:46:48 +0900

    feat: Swiper 컴포넌트 추가

--- src/components/SwiperSlider.tsx
+++ src/components/SwiperSlider.tsx
@@ -1,8 +1,45 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, A11y, Autoplay } from 'swiper/modules';
+import movieListData from '../data/movieListData.json';
+import { baseUrl } from '../constants/api';
+import 'swiper/css';
+
 const SwiperSlider = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <Swiper
+      modules={[Pagination, A11y, Autoplay]}
+      slidesPerView={3}
+      loop
+      navigation
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      breakpoints={{
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+        1280: {
+          slidesPerView: 5,
+        },
+      }}
+    >
+      {movieListData.results.map((movie, index) => (
+        <SwiperSlide key={movie.id}>
+          <div>
+            <img src={`${baseUrl}${movie.poster_path}`} alt={movie.title} className="w-80" />
+            Add commentMore actions
+            <h1>
+              Top <span>{index + 1}</span>
+            </h1>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 export default SwiperSlider;
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,20 +1,24 @@
 import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import movieListData from '../data/movieListData.json';
+import SwiperSlider from '../components/SwiperSlider';
 
 const MovieList = () => {
   return (
-    <div className="flex flex-wrap justify-center items-center gap-8">
-      {movieListData.results.map(movie => (
-        <Link to={`/details/${movie.id}`} key={movie.id}>
-          <MovieCard
-            title={movie.title}
-            poster_path={movie.poster_path}
-            vote_average={movie.vote_average}
-          />
-        </Link>
-      ))}
-    </div>
+    <main>
+      <SwiperSlider />
+      <div className="flex flex-wrap justify-center items-center gap-8">
+        {movieListData.results.map(movie => (
+          <Link to={`/details/${movie.id}`} key={movie.id}>
+            <MovieCard
+              title={movie.title}
+              poster_path={movie.poster_path}
+              vote_average={movie.vote_average}
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
   );
 };
 
